379053790080df1271c674e35f176dd182fbfb97
Lara Isak <luckasti.larry@gmail.com>
2021-05-19 19:29:09 +0200

    🎨 exercise 1.2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -8,21 +8,23 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {  
   return (
     <>
-      <p>Course consists of mutiple parts each with its own set of exercises:</p>
-      <p>
-        {props.part1} - Number of exercises: {props.exercises1}
-      </p>
-      <p>
-        {props.part2} - Number of exercises: {props.exercises2}
-      </p>
-      <p>
-        {props.part3} - Number of exercises: {props.exercises3}
-      </p>
+      <p>{props.part} - Number of exercises: {props.exercise}</p>
     </>
   )
+}
+
+const Content = (props) => {
+return (
+  <>
+    <p>Course consists of mutiple parts each with its own set of exercises:</p>
+    <Part part={props.part1} exercise={props.exercises1}/>
+    <Part part={props.part2} exercise={props.exercises2}/>
+    <Part part={props.part3} exercise={props.exercises3}/>
+  </>
+)
 }
 
 const Total = (props) => {
